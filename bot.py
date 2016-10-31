e6655e771cf627634a79f8ddfdda367b5aadba35
Hobson Lane <github@totalgood.com>
2016-10-30 20:01:28 -0700

    Reply instances should be created, but none captured so far

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ class Bot:
 
         return tweet_list
 
-    def _filter_harsh(self, tweet, tag):
+    def _is_acceptable(self, tweet, tag, picky=False):
         """
         @brief     This will pull off hash tags just at the end of
                    tweet.  If your tag is not in the ending list
@@ -56,35 +56,48 @@ class Bot:
         @return     the tweet, minus ending tags and the list of tags
                     or False
         """
-        if 'http' in tweet.text:
+        if picky and 'http' in tweet.text:
             return False
         else:
-            # tag_list = [d['text'] for d in tweet.hashtags]
-            tweet_list = tweet.text.split()
-            tag_list = []
-            for word in reversed(tweet_list):
-                if word[0] == "#":
-                    tag_list.append(word[1:].lower())
-                else:
-                    break
+            if picky:
+                tweet_list = tweet.text.split()
+                tag_list = []
+                for word in reversed(tweet_list):
+                    if word[0] == "#":
+                        tag_list.append(word[1:].lower())
+                    else:
+                        break
+            else:
+                tag_list = [d['text'] for d in tweet.entities.get('hashtags', [])]
             if tag not in tag_list:
                 return False
+
         # length = len(tweet_list) - len(tag_list)
         # tagless_list = tweet_list[:length]
         # tweet.tag_list = tag_list
         # tweet.filtered_text = " ".join(tagless_list)  # .replace("#", "")
-        user = model.User.create_or_get(screen_name=tweet.user.screen_name,
-                                        followers_count=tweet.user.followers_count,
-                                        statuses_count=tweet.user.statuses_count,
-                                        friends_count=tweet.user.friends_count,
-                                        )
-        tweet = model.Tweet.create_or_get(  # id=tweet.id,
-                                          id_str=tweet.id_str,
-                                          user=user,
-                                          # favourites_count=tweet.favourites_count,
-                                          text=tweet.text,
-                                          tags=' '.join(sorted(tag_list)))
-        return tweet
+        user, created = model.User.create_or_get(
+            screen_name=tweet.user.screen_name,
+            followers_count=tweet.user.followers_count,
+            statuses_count=tweet.user.statuses_count,
+            friends_count=tweet.user.friends_count,
+            favourites_count=tweet.user.favourites_count,
+            )
+        tweet_obj, created = model.Tweet.create_or_get(  # id=tweet.id,
+            id=tweet.id,
+            id_str=tweet.id_str,
+            user=user,
+            favorite_count=tweet.favorite_count,
+            text=tweet.text,
+            tags=' '.join(sorted(tag_list)))
+        id_str = tweet.in_reply_to_status_id_str
+        if id_str:
+            tweet_replied_to, created = model.Tweet.create_or_get(id_str=id_str)
+            if created:
+                print("We don't have the tweet ({}) that this ({}) was a reply to , but we could GET it ;)".format(
+                    id_str, tweet.id_str))
+            model.Reply.create_or_get(tweet=tweet_obj, tweet_replied_to=tweet_replied_to)
+        return tweet_obj
 
     def clean_tweet(self, tweet):
         """ Strip # character and @usernames out of tweet """
@@ -99,8 +112,14 @@ class Bot:
 
 if __name__ == '__main__':
     args = sys.argv
-    num_tweets, delay = None, None
+    num_tweets, delay, picky = None, None, None
+    # --picky flag means to ignore any tweets that contain "http" and does not end with one of the desired hashtags
+    if '--picky' in args:
+        del args[args.index('--picky')]
+        picky = True
     hashtags = []
+    # the first float found on the command line is the delay in seconds between twitter search queries
+    # the first int after the first float is the number of tweets to retrieve with each twitter search query
     for arg in args[1:]:
         try:
             num_tweets = int(arg) if not num_tweets else int('unintable')
@@ -121,12 +140,14 @@ if __name__ == '__main__':
     while True:
         num_before = bot.count()
         for ht in hashtags:
+            print('=' * 80)
+            print('Looking for #{}'.format(ht))
             last_tweets = []
             for tweet in bot.tag_search(ht, num_tweets):
-                acceptable_tweet = bot._filter_harsh(tweet, ht)
+                acceptable_tweet = bot._is_acceptable(tweet, ht, picky=picky)
                 if acceptable_tweet:
                     last_tweets += [acceptable_tweet]
-            print(json.dumps(last_tweets, indent=2))
+            print(json.dumps(last_tweets, default=model.Serializer(), indent=2))
             time.sleep(max(random.gauss(delay, delay_std), min_delay))
 
         num_after = bot.count()
